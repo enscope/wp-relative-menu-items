e0e007de0d2721111e5070398c863bcfa5a88fb1
Miroslav Hudak <mhudak@dev.enscope.com>
2015-04-16 13:19:14 +0200

    Added plugin source

---
+++ relative-menu-items.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Plugin Name: Relative Menu Items
+    Description: Allows to include relative ROOT in menu items
+    Version: 1.0
+    Author: Miroslav Hudak (enscope, s.r.o.)
+    Author URI: http://www.enscope.com
+*/
+
+add_filter('wp_nav_menu_items', function($items, $args) {
+    // trim trailing slash and consider multi-site setup
+    $rootUrl = rtrim(is_multisite() ? network_site_url() : home_url(), '/');
+    return preg_replace('/https?:\/\/%ROOT%/i', $rootUrl, $items);
+}, 10, 2);
